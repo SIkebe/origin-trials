72d3593209bcfd822845e846b0dbac9c653cbccc
SIkebe <SIkebe@users.noreply.github.com>
2022-09-28 15:58:50 +0900

    popup

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
 # origin-trials
-ASP.NET Core application for testing origin trial features.
+ASP.NET Core application for testing origin trial features.  
+https://origin-trials.azurewebsites.net/
 
 ## Prerequisite
 * .NET 7.0
---
+++ src/OriginTrials/Pages/Popup.cshtml.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace OriginTrials.Pages;
+
+public class PopupModel : PageModel
+{
+    private readonly ILogger<PopupModel> _logger;
+
+    public PopupModel(ILogger<PopupModel> logger)
+    {
+        _logger = logger;
+    }
+
+    public void OnGet()
+    {
+    }
+}
